6feb2e4d863b3c184d095acda020296a307a60b4
Andrey Zamyatin <zamatin12@gmail.com>
2025-02-15 21:02:37 +0500

    первый шаблон программы

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("hellow, world")
-"да"
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MyApp(App):
+    def build(self):
+        layout = BoxLayout()
+        btn = Button(text="Нажми меня!")
+        btn.bind(on_press=self.on_button_click)
+        layout.add_widget(btn)
+        return layout
+
+    def on_button_click(self, instance):
+        instance.text = "Нажата!"
+
+
+if __name__ == "__main__":
+    MyApp().run()
